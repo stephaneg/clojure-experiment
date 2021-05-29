e0c7cbb25f1f1e11834e8a2ff978aea10fcdd177
Stephane Gesuati <stephstephg@gmail.com>
2021-05-29 14:42:53 +0200

    small changes

--- src/clojure_experiment/game-of-life.clj
+++ src/clojure_experiment/game-of-life.clj
@@ -14,4 +14,11 @@
 
 (def glider (populate (empty-board 6 6) #{[2 0] [2 1] [2 2] [1 2] [0 1]}))
 
+(pprint glider)
+
+(defn neighbours
+  [[x y]]
+  (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)] [(+ dx x) (+ dy y)]))
+
+(neighbours [[1 1]])
 
